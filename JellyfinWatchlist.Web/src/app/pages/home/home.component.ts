a657570b0c7710c6ea04e7d9c231b5961ef3cab2
mzrimsek <github@zrimsek.com>
2025-02-22 05:23:15 -0500

    basic jellyfin auth works

--- JellyfinWatchlist.Web/src/app/pages/home/home.component.ts
+++ JellyfinWatchlist.Web/src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { JellyfinService } from '../../services/jellyfin.service';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  username: string = '';
+  constructor(private jellyfinService: JellyfinService) {}
 
+  ngOnInit(): void {
+    this.jellyfinService.getCurrentUser().then((user) => {
+      this.username = user.Name ?? 'User';
+    });
+  }
 }
