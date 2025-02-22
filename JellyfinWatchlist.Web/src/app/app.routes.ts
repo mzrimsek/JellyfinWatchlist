70d20fa080d9c4a08d0553bc52221bc0e37d063a
mzrimsek <github@zrimsek.com>
2025-02-22 04:32:52 -0500

    add basic page scaffolding with rudimentary auth guard

--- JellyfinWatchlist.Web/src/app/app.routes.ts
+++ JellyfinWatchlist.Web/src/app/app.routes.ts
@@ -1,3 +1,21 @@
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: LoginComponent,
+    path: 'login',
+  },
+  {
+    component: HomeComponent,
+    path: '',
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
---
+++ JellyfinWatchlist.Web/src/app/services/jellyfin.service.ts
@@ -0,0 +1,40 @@
+import { Api, Jellyfin } from '@jellyfin/sdk';
+
+import { Injectable } from '@angular/core';
+import { UserDto } from '@jellyfin/sdk/lib/generated-client/models';
+import { environment } from '../../environments/environment';
+import { getUserApi } from '../../../node_modules/@jellyfin/sdk/lib/utils/api/user-api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JellyfinService {
+  private sdk = new Jellyfin({
+    clientInfo: {
+      name: 'Jellyfin Watchlist',
+      version: '1.0.0',
+    },
+    deviceInfo: {
+      name: 'Jellyfin Watchlist',
+      id: 'JellyfinWatchlist',
+    },
+  });
+  private api: Api;
+  constructor() {
+    this.api = this.sdk.createApi(environment.jellyfin.baseUrl);
+  }
+
+  public async login(username: string, password: string): Promise<void> {
+    await this.api.authenticateUserByName(username, password);
+  }
+
+  public get isAuthenticated(): boolean {
+    return !!this.api.accessToken;
+  }
+
+  public async getCurrentUser(): Promise<UserDto> {
+    const userApi = getUserApi(this.api);
+    const response = await userApi.getCurrentUser();
+    return response.data;
+  }
+}
