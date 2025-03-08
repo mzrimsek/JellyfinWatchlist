4ff2be9c3ccb0320329ddcbe181f4a34054a8099
mzrimsek <github@zrimsek.com>
2025-03-08 13:22:50 -0500

    add hmr for development

--- JellyfinWatchlist.Api/src/main.ts
+++ JellyfinWatchlist.Api/src/main.ts
@@ -3,9 +3,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
+declare const module: {
+  hot: { accept: () => void; dispose: (callback: () => any) => void };
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('JellyfinWatchlist API')
     .setDescription('The JellyfinWatchlist API')
@@ -15,6 +20,13 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
