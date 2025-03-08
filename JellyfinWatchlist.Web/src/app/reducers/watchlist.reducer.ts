f141c961be3dc7bc49d10b9da187b21021934fca
mzrimsek <github@zrimsek.com>
2025-03-07 23:31:34 -0500

    add watchlist reducer

---
+++ JellyfinWatchlist.Web/src/app/reducers/watchlist.reducer.ts
@@ -0,0 +1,44 @@
+import * as WatchlistActions from '../actions/watchlist.actions';
+
+import { EntityAdapter, EntityState, createEntityAdapter } from '@ngrx/entity';
+import { createReducer, on } from '@ngrx/store';
+
+import { MediaItem } from '../shared/models';
+
+export interface State extends EntityState<MediaItem> {}
+
+export function selectMediaItemId(item: MediaItem): string {
+  return item.id;
+}
+
+export function sortByName(a: MediaItem, b: MediaItem): number {
+  return a.name.localeCompare(b.name);
+}
+
+export const adapter: EntityAdapter<MediaItem> = createEntityAdapter<MediaItem>(
+  {
+    selectId: selectMediaItemId,
+    sortComparer: sortByName,
+  }
+);
+
+export const initialState: State = adapter.getInitialState();
+
+export const watchlistReducer = createReducer(
+  initialState,
+  on(WatchlistActions.watchlistAddItem, (state, { item }) =>
+    adapter.addOne(item, state)
+  ),
+  on(WatchlistActions.watchlistRemoveItem, (state, { item }) =>
+    adapter.removeOne(item.id, state)
+  ),
+  on(WatchlistActions.watchlistClear, (state) => adapter.removeAll(state))
+);
+
+export const { selectIds, selectEntities, selectAll, selectTotal } =
+  adapter.getSelectors();
+
+export const selectWatchlistIds = selectIds;
+export const selectWatchlistEntities = selectEntities;
+export const selectAllWatchlist = selectAll;
+export const selectWatchlistTotal = selectTotal;
