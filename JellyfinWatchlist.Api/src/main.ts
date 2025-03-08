0ff9902c109897611f1c3c7456547babceac9432
mzrimsek <github@zrimsek.com>
2025-03-08 13:03:11 -0500

    add swagger

--- JellyfinWatchlist.Api/src/main.ts
+++ JellyfinWatchlist.Api/src/main.ts
@@ -1,8 +1,20 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('JellyfinWatchlist API')
+    .setDescription('The JellyfinWatchlist API')
+    .setVersion('1.0')
+    .addTag('Jellyfin')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
