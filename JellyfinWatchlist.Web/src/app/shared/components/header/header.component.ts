1aab4fe43feb8ef794eed2710f6b4e60dbae26a1
mzrimsek <github@zrimsek.com>
2025-03-07 22:28:15 -0500

    add logout button to the header to invoke the logout stuff that already exists

--- JellyfinWatchlist.Web/src/app/shared/components/header/header.component.ts
+++ JellyfinWatchlist.Web/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,22 @@
+import * as authActions from '../../../actions/auth.actions';
+
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-shared-header',
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private store: Store) {}
+
+  logout() {
+    this.store.dispatch(authActions.logout());
+  }
+}
