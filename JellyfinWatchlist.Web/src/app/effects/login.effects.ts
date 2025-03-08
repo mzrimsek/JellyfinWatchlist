928316072dc513d8395be8ccc55a807bb3381682
mzrimsek <github@zrimsek.com>
2025-03-07 19:25:49 -0500

    fixed effect not getting registered

--- JellyfinWatchlist.Web/src/app/effects/login.effects.ts
+++ JellyfinWatchlist.Web/src/app/effects/login.effects.ts
@@ -1,6 +1,6 @@
 import * as jellyfinActions from '../actions/jellyfin.actions';
 
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Injectable, inject } from '@angular/core';
 import { catchError, exhaustMap, map, of } from 'rxjs';
 
@@ -11,19 +11,21 @@ export class LoginEffects {
   private actions$ = inject(Actions);
   private jellyfinService = inject(JellyfinService);
 
-  loginActions$ = this.actions$.pipe(
-    ofType(jellyfinActions.login),
-    exhaustMap((action) =>
-      this.jellyfinService.login(action.username, action.password).pipe(
-        map((success) => {
-          if (success) {
-            return jellyfinActions.loginSucceeded;
-          } else {
-            return jellyfinActions.loginFailed;
-          }
-        }),
-        catchError(() => of(jellyfinActions.loginFailed))
+  loginActions$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(jellyfinActions.login),
+      exhaustMap((action) =>
+        this.jellyfinService.login(action.username, action.password).pipe(
+          map((success) => {
+            if (success) {
+              return jellyfinActions.loginSucceeded();
+            } else {
+              return jellyfinActions.loginFailed();
+            }
+          }),
+          catchError(() => of(jellyfinActions.loginFailed()))
+        )
       )
-    )
-  );
+    );
+  });
 }
