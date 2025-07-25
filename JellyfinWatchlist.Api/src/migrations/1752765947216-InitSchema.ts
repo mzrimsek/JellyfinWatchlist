dc5f1356119408f52b6a24e162e1a7e8434e5538
Mike Zrimsek <github@zrimsek.com>
2025-07-24 20:06:19 -0400

    Feature/improved dev exp (#18)
    * unify tsconfigs
    
    * update gitignore
    
    * Update README and add package.json and package-lock.json for unified development setup
    
    * Add Prettier configuration and update workflows for consistent formatting
    
    - Introduced .editorconfig and .prettierrc for unified code style
    - Added .prettierignore to exclude unnecessary files from formatting
    - Updated package.json to include Prettier as a dev dependency
    - Modified CI workflows to use single quotes for consistency
    - Removed redundant .prettierrc from JellyfinWatchlist.Api
    - Created workspace configuration for easier project navigation
    
    * add workspace
    
    * add api launch configs
    
    * update readme

--- JellyfinWatchlist.Api/src/migrations/1752765947216-InitSchema.ts
+++ JellyfinWatchlist.Api/src/migrations/1752765947216-InitSchema.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InitSchema1752765947216 implements MigrationInterface {
-    name = 'InitSchema1752765947216'
+  name = 'InitSchema1752765947216';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "watchlist_item" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "mediaType" varchar NOT NULL, "year" integer NOT NULL, "primaryImageUrl" varchar NOT NULL, "jellyfinUserId" varchar NOT NULL, "addedOn" datetime NOT NULL)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "watchlist_item" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "mediaType" varchar NOT NULL, "year" integer NOT NULL, "primaryImageUrl" varchar NOT NULL, "jellyfinUserId" varchar NOT NULL, "addedOn" datetime NOT NULL)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "watchlist_item"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "watchlist_item"`);
+  }
 }
